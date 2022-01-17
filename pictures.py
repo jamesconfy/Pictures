4f7c3af4197c544ef5ea0d55de58022c3ab6e813
jamesconfy <bobdence@gmail.com>
2022-01-17 13:58:24 +0100

    Created delete and modify routes

--- pictures.py
+++ pictures.py
@@ -40,15 +40,46 @@ def uploadImage():
             pic = ImageFile(image_file=imgFile)
             db.session.add(pic)
             db.session.commit()
-            flash(f'Image Uploaded Successfully')
+            flash(f'Image Uploaded Successfully', 'success')
+            return redirect(url_for('home'))
+
+        flash(f'The file is invalid', 'danger')
+    return render_template("upload.html", form=form, title="Upload Image")
+
+@app.route("/view/<int:id>")
+def viewImage(id):
+    image = ImageFile.query.get_or_404(id)
+    return render_template('view.html', image=image, title=image.id)
+
+@app.route("/delete/<id>")
+def deleteImage(id):
+    image = ImageFile.query.get_or_404(id)
+
+    db.session.delete(image)
+    db.session.commit()
+    flash(f'Image Deleted Successfully', 'info')
+    return redirect(url_for('home'))
+
+@app.route("/modify/<int:id>", methods=["POST", "GET"])
+def modifyImage(id):
+    image = ImageFile.query.get_or_404(id)
+
+    form = ImageForm()
+    if request.method =='POST':
+        if form.validate_on_submit():
+            imgFile = form.img.data
+            if imgFile:
+                imgFile2 = save_image(form.img.data)
+            else:
+                imgFile2 = image.image_file
+
+            image.image_file = imgFile2
+            db.session.commit()
+            flash(f'Image Modified Successfully', 'success')
             return redirect(url_for('home'))
 
         flash(f'The file {form.img.data} is invalid')
-    return render_template("upload.html", form=form, title="Upload Image")
-
-@app.route("/delete")
-def deleteImage():
-    return render_template('home.html')
+    return render_template("upload.html", form=form, title="Modify Image")
 
 if __name__ == "__main__":
     app.run(db.create_all())
